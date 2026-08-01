db13bc11a636b8ee973dc1d6e4391dbff25464cb
Gugilla Aakash <gugillaaakash6@gmail.com>
2026-08-01 21:58:52 +0530

    feat(core): introduce CircuitOpenError exception

--- hakiapi/core/circuit_breaker.py
+++ hakiapi/core/circuit_breaker.py
@@ -1,7 +1,23 @@
 from enum import Enum
+from typing import Any
+
+from .exceptions import HakiAPIError
 
 
 class CircuitState(Enum):
     CLOSED = "CLOSED"
     OPEN = "OPEN"
     HALF_OPEN = "HALF_OPEN"
+
+
+class CircuitOpenError(HakiAPIError):
+    """Raised when requests are blocked because the circuit is open."""
+
+    def __init__(
+        self,
+        message: str = "Circuit breaker is OPEN.",
+        retry_after: float | None = None,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(message=message, **kwargs)
+        self.retry_after = retry_after
