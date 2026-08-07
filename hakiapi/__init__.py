ce2efc509ebe4aae54a1c45f4c44b208816db9f2
Gugilla Aakash <gugillaaakash6@gmail.com>
2026-08-07 21:51:51 +0530

    feat: add package __version__ support

--- hakiapi/__init__.py
+++ hakiapi/__init__.py
@@ -4,11 +4,16 @@ from .core.base_client import BaseAPIClient
 from .clients.github import GitHubClient
 from .clients.gmail import GmailClient
 from .clients.google_calendar import GoogleCalendarClient
-
+from importlib.metadata import PackageNotFoundError, version
 from .core import exceptions
 from .core import auth
 from .core import retry
 from .core import paginator
+
+try:
+    __version__ = version("hakiapi")
+except PackageNotFoundError:
+    __version__ = "0.1.0"
 
 __all__ = [
     "BaseAPIClient",
@@ -19,4 +24,5 @@ __all__ = [
     "auth",
     "retry",
     "paginator",
+    "__version__",
 ]
